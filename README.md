078188e951a3b83219c1e94c2026158e261999bf
Harsh Valecha <harsh.valecha@yahoo.com>
2026-08-30 08:07:07 +0530

    Added more questions and solutions

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Basic Python Interview Preparation Kit
 
-### Topics
+### Questions List -
 
 ---
 
@@ -9,6 +9,16 @@
 3. Write your Full name , reverse the firstname only and print the fullname
 4. Join a list of values in semicolon delimited way
 5. Remove duplicates from a list
+6. write a program to check if a string is palindrome or not
+7. remove duplicates from string while preserving the order
+8. Count occurences of a character in a string
+9. Anagrams : Check if 2 different string have same occurences of characters
+10. Remove spaces from a string - starting , ending , both
+11. check if a string contains only digits
+12. Give first non repeating character in a string
+13. Check if a string starts with a prefix or a suffix
+
+---
 
 ### Best Way for you to practice -
 
--- string_ops.py
+++ string_ops.py
@@ -2,10 +2,64 @@
 # first name in reverse + space + lastname
 full_name = 'Jaskirat Rangi'
 firstname,lastname = full_name.split(' ')
+
+# 3 ways to do it  - 
 print(firstname[len(firstname)::-1]+' '+lastname)
+print(firstname[::-1]+' '+lastname)
 print(''.join(reversed(firstname))+' '+lastname)
 
 
 # join
 values = ['kale','lafi','zoro','nami','sanji','pickle']
 print(';'.join(values))
+
+
+# palindrome
+val = 'racecar'
+def is_palindrome(value):
+    return value==value[::-1]
+print(is_palindrome('racecar'))
+print(is_palindrome('jethalal'))
+
+# Remove duplicates from a string while preserving order
+s = 'hululululujafar'
+print(''.join(dict.fromkeys(s)))
+
+# count occurences of characters in a string
+from collections import Counter
+print(Counter(s))
+
+# Anagrams
+def is_anagram(str1,str2):
+    return Counter(str1.lower()) == Counter(str2.lower())
+
+print(is_anagram('silent','listen'))
+print(is_anagram('luffy','sanji'))
+
+# Remove Spaces
+str = '   KOOOOOL FULLTOOSH  '
+print(str.lstrip()) # left space removing
+print(str.rstrip()) # right space removing
+print(str.strip()) # remove from both left and right 
+
+
+# only digits in string
+number = '987654321'
+print(number.isdigit())
+
+
+# first non repeating character in a string 
+def first_unique(s):
+    counts = Counter(s)
+    for char in s:
+        if counts[char]==1:
+            return char
+    return None
+
+print(first_unique('jameeljamali'))
+
+# check if a string starts with a prefix
+print('Mr. Oliva'.startswith('Mr.'))
+
+# starts with suffix
+print('Baki Hanma'.endswith('Hanma'))
